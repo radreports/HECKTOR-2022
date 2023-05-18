6613a11b7b6bf84cd6125b03ce1e725c4e77a98a
Anil Yerramasu <anilyerramasu@Anils-MacBook-Pro.local>
2023-05-18 18:51:50 +0530

    addede requirements

--- Task 1/rename_image.py
+++ Task 1/rename_image.py
@@ -5,7 +5,7 @@ This file renames PET/CT images to match the name convention used for nnU-Net.
 import os
 join = os.path.join
 
-root_path = "path/to/images"
+root_path = "/home/ubuntu/storage1/HECKTOR-2022/data/hecktor2022_training/hecktor2022"
 
 if os.path.exists(join(root_path, '.DS_Store')):
     os.remove(join(root_path, '.DS_Store'))
