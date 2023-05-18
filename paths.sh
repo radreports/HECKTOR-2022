27d3d8c236b9e0cf1665ad5dc08b821375d9aaaa
Anil Yerramasu <anilyerramasu@Anils-MacBook-Pro.local>
2023-05-18 20:35:47 +0530

    addede requirements

---
+++ paths.sh
@@ -0,0 +1,4 @@
+export nnUNet_raw_data_base="/home/ubuntu/storage1/HECKTOR-2022/nnUNet_raw/"
+export nnUNet_preprocessed="/home/ubuntu/storage1/HECKTOR-2022/nnUNet_preprocessed"
+#results_folder2="/content/drive/MyDrive/HECKTOR2022/DATA/results_folder2" #change to dif folder when train on CT only
+export RESULTS_FOLDER="/home/ubuntu/storage1/HECKTOR-2022/RESULTS_FOLDER"
